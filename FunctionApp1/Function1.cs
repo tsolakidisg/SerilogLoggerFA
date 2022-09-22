867dfe070423d2e0c45c882bf97c9a7df30b18bc
Grigoris Tsolakidis <gtsolakidis@deloitte.gr>
2022-09-22 18:06:50 +0300

    Update for conditional output writing.

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -8,25 +9,32 @@ namespace FunctionApp1
 {
     public class Function1
     {
-        private readonly ILogger _logger;
+        //private readonly ILogger _logger;
 
-        public Function1(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<Function1>();
-        }
+        //public Function1(ILoggerFactory loggerFactory)
+        //{
+        //    _logger = loggerFactory.CreateLogger<Function1>();
+        //}
 
         [Function("Function1")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req, FunctionContext context)
         {
             //_logger.LogInformation("C# HTTP trigger function processed a request.");
             var logger = context.GetLogger<Function1>();
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            logger.LogDebug("C# HTTP trigger function processed a request.");
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
             response.WriteString("Welcome to Azure Functions!");
+            string useCase = "New Order";
+            string orderStatus = "Received";
+            var guid = Guid.NewGuid();
+            var service = req.Url;
+            string levelType = "HttpRequest";
 
-            logger.LogInformation(response.Body.ToString());
+            logger.LogInformation("{GLOBAL_UUID}{USE_CASE}{ORDER_STATUS}{TIMESTAMP}", guid, useCase, orderStatus, DateTime.Now);
+            logger.LogWarning("{GLOBAL_UUID}{USE_CASE}{REQUEST_UUID}{SERVICE}{END_SYSTEM}{STATE}{PAYLOAD}{TIMESTAMP}{LEVEL_TYPE}", 
+                                guid, useCase, Guid.NewGuid(), service, context.GetType, orderStatus, req.Body, DateTime.Now, levelType);
 
             return response;
         }
--- FunctionApp1/Program.cs
+++ FunctionApp1/Program.cs
@@ -2,7 +2,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 using Serilog.Sinks.MSSqlServer;
+using Serilog.Templates;
 using System.Diagnostics;
 
 var host = new HostBuilder()
@@ -10,23 +13,138 @@ var host = new HostBuilder()
     .ConfigureServices(s =>
     {
         var dbConnection = Environment.GetEnvironmentVariable("LogConnection", EnvironmentVariableTarget.Process);
-        var sinkOpts = new MSSqlServerSinkOptions();
-        sinkOpts.TableName = "Logs";
-        sinkOpts.AutoCreateSqlTable = true;
-        var columnOpts = new ColumnOptions();
-        columnOpts.Store.Remove(StandardColumn.Properties);
-        columnOpts.Store.Add(StandardColumn.LogEvent);
-        columnOpts.LogEvent.DataLength = 2048;
-        columnOpts.PrimaryKey = columnOpts.TimeStamp;
-        columnOpts.TimeStamp.NonClusteredIndex = true;
+
+        // HEAD_MONITOR_TABLE Options and Configuration
+        var sinkOptsHead = new MSSqlServerSinkOptions
+        {
+            TableName = "HEAD_MONITOR_TABLE",
+            AutoCreateSqlTable = true
+        };
+
+        var columnOptsHead = new ColumnOptions();
+
+        // Override the default Primary Column of Serilog by custom column
+        columnOptsHead.Id.ColumnName = "GLOBAL_UUID";
+
+        // Removing all the unencessary columns
+        columnOptsHead.Store.Remove(StandardColumn.Id);
+        columnOptsHead.Store.Remove(StandardColumn.Message);
+        columnOptsHead.Store.Remove(StandardColumn.MessageTemplate);
+        columnOptsHead.Store.Remove(StandardColumn.Level);
+        columnOptsHead.Store.Remove(StandardColumn.TimeStamp);
+        columnOptsHead.Store.Remove(StandardColumn.Exception);
+        columnOptsHead.Store.Remove(StandardColumn.Properties);
+
+        // Adding all the custom columns
+        columnOptsHead.AdditionalColumns = new List<SqlColumn>
+        {
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "GLOBAL_UUID", DataLength = 50, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "USE_CASE", DataLength = 255, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "ORDER_STATUS", DataLength = 50, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.DateTime, ColumnName = "TIMESTAMP", DataLength=-1, AllowNull = false}
+        };
+
+        // DETAILS_MONITOR_TABLE Options and Configuration
+        var sinkOptsDetails = new MSSqlServerSinkOptions
+        {
+            TableName = "DETAILS_MONITOR_TABLE",
+            AutoCreateSqlTable = true
+        };
+
+        var columnOptsDetails = new ColumnOptions();
+
+        // Override the default Primary Column of Serilog by custom column
+        //columnOptsDetails.Id.ColumnName = "REQUEST_UUID";
+        //columnOptsDetails.PrimaryKey.ColumnName = "REQUEST_UUID";
+        //columnOptsDetails.PrimaryKey.ColumnName = "STATE";
+        //columnOptsDetails.PrimaryKey.PropertyName = "PrimaryKey";
+
+        // Removing all the unencessary columns
+        columnOptsDetails.Store.Remove(StandardColumn.Id);
+        columnOptsDetails.Store.Remove(StandardColumn.Message);
+        columnOptsDetails.Store.Remove(StandardColumn.MessageTemplate);
+        columnOptsDetails.Store.Remove(StandardColumn.Level);
+        columnOptsDetails.Store.Remove(StandardColumn.TimeStamp);
+        columnOptsDetails.Store.Remove(StandardColumn.Exception);
+        columnOptsDetails.Store.Remove(StandardColumn.Properties);
+
+        // Adding all the custom columns
+        columnOptsDetails.AdditionalColumns = new List<SqlColumn>
+        {
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "GLOBAL_UUID", DataLength = 50, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "USE_CASE", DataLength = 255, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "REQUEST_UUID", DataLength = 50, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "SERVICE", DataLength = 255, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "END_SYSTEM", DataLength = 50, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "STATE", DataLength = 50, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "PAYLOAD", DataLength=-1, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.DateTime, ColumnName = "TIMESTAMP", DataLength=-1, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "LEVEL_TYPE", DataLength = 50, AllowNull = false},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "EXCEPTION", DataLength=-1, AllowNull = true},
+            new SqlColumn { DataType = System.Data.SqlDbType.NVarChar, ColumnName = "STACK_TRACE", DataLength=-1, AllowNull = true}
+        };
+
         var logger = new LoggerConfiguration()
-        .Enrich.WithProperty("ApplicationContext", "FunctionApp1")
-        .WriteTo.Console()
-        .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
-        .WriteTo.MSSqlServer(
-            connectionString: dbConnection,
-            sinkOptions: sinkOpts,
-            columnOptions: columnOpts)
+        .Enrich.WithProperty("ApplicationContext", "Mediator")
+        .MinimumLevel.Debug()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .MinimumLevel.Override("Worker", LogEventLevel.Warning)
+        .MinimumLevel.Override("Host", LogEventLevel.Warning)
+        .MinimumLevel.Override("Host.Aggregator", LogEventLevel.Warning)
+        .MinimumLevel.Override("Host.Results", LogEventLevel.Warning)
+        .MinimumLevel.Override("System", LogEventLevel.Error)
+        .MinimumLevel.Override("Function", LogEventLevel.Error)
+        .MinimumLevel.Override("Azure.Storage.Blobs", LogEventLevel.Error)
+        .MinimumLevel.Override("Azure.Core", LogEventLevel.Error)
+        .MinimumLevel.Override("Microsoft.Azure.WebJobs", LogEventLevel.Error)
+        .MinimumLevel.Override("Microsoft.Azure.Functions.Worker", LogEventLevel.Error)
+        .MinimumLevel.Override("Microsoft.Azure.Functions.Worker.Http", LogEventLevel.Error)
+        .WriteTo.Logger(l =>
+        {
+            l.WriteTo.Conditional(ev =>
+            {
+                bool isDebug = ev.Level == LogEventLevel.Debug;
+                if (isDebug) { return true; }
+                return false;
+            }, wt => wt.Console());
+        })
+        .WriteTo.Logger(l =>
+        {
+            l.WriteTo.Conditional(ev =>
+            {
+                bool isDebug = ev.Level == LogEventLevel.Debug;
+                if (isDebug) { return true; }
+                return false;
+            }, wt => wt.File(
+                    "log.txt",
+                    rollingInterval: RollingInterval.Day));
+        })
+        .WriteTo.Logger(l => 
+        {
+            l.WriteTo.Conditional(ev =>
+            {
+                bool isInformation = ev.Level == LogEventLevel.Information;
+                if (isInformation) { return true; }
+                return false;
+            },
+            wt => wt.MSSqlServer(
+                    connectionString: dbConnection,
+                    sinkOptions: sinkOptsHead,
+                    columnOptions: columnOptsHead));
+        })
+        .WriteTo.Logger(l =>
+        {
+            l.WriteTo.Conditional(ev =>
+            {
+                bool isInformation = ev.Level == LogEventLevel.Information;
+                if (isInformation) { return true; }
+                return false;
+            },
+            wt => wt.MSSqlServer(
+                    connectionString: dbConnection,
+                    sinkOptions: sinkOptsDetails,
+                    columnOptions: columnOptsDetails));
+        })
         .CreateLogger();
         s.AddLogging(lb =>
         {
@@ -40,5 +158,7 @@ var host = new HostBuilder()
         });
     })
     .Build();
+
+
 
 host.Run();
